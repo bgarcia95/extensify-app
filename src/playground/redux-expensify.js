f182cc4a45ccec8203d68110ba4faad27f777dc3
Brayan García <bgarcia.95@outlook.com>
2019-12-03 15:17:53 -0600

    adding sorting in redux. Redux module completed.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -128,6 +128,38 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
   }
 };
 
+// Timestamps -> counting in milliseconds
+// January 1st 1970 (unix epoch) -> this is the value set when timestamp is set to 0
+// When a positive number is set the date is forward the one when the value is set to 0 and if negative, it goes backwards
+// 33400 (33.4s after the previous date), 10, -203
+
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses
+    .filter(expense => {
+      const startDateMatch =
+        typeof startDate !== "number" || expense.createdAt >= startDate;
+      const endDateMatch =
+        typeof endDate !== "number" || expense.createdAt <= endDate;
+
+      const textMatch = expense.description
+        .toLowerCase()
+        .includes(text.toLowerCase());
+
+      return startDateMatch && endDateMatch && textMatch;
+    })
+    .sort((a, b) => {
+      if (sortBy === "date") {
+        return a.createdAt < b.createdAt ? 1 : -1;
+      } else if (sortBy === "amount") {
+        return a.amount < b.amount ? 1 : -1;
+      }
+
+      // sortBy -> amount
+      // put the ones with a greater amount first
+    });
+};
+
 // Store Creation
 
 const store = createStore(
@@ -138,23 +170,27 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-  console.log(store.getState());
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
 });
 
 // Adding expenses
-// const expenseOne = store.dispatch(
-//   addExpense({
-//     description: "Rent",
-//     amount: 100
-//   })
-// );
+const expenseOne = store.dispatch(
+  addExpense({
+    description: "Rent",
+    amount: 100,
+    createdAt: -21000
+  })
+);
 
-// const expenseTwo = store.dispatch(
-//   addExpense({
-//     description: "Coffee",
-//     amount: 300
-//   })
-// );
+const expenseTwo = store.dispatch(
+  addExpense({
+    description: "Coffee",
+    amount: 300,
+    createdAt: -1000
+  })
+);
 
 // Removing Expenses
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
@@ -166,17 +202,17 @@ store.subscribe(() => {
 // store.dispatch(setTextFilter("rent"));
 
 // Sorting by amount
-// store.dispatch(sortByAmount()); // sortBy: amount
+store.dispatch(sortByAmount()); // sortBy: amount
 
 // Sorting by date
 // store.dispatch(sortByDate()); // sortBy: date
 
 // Setting startDate
-store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
+// store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate());
 
 // Setting endDate
-store.dispatch(setEndDate(1250));
+// store.dispatch(setEndDate(999));
 
 const demoState = {
   expenses: [
