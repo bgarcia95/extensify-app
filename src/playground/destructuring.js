056aea26963ec91770459d495e08f8b02d2e0ee9
Brayan García <bgarcia.95@outlook.com>
2019-12-02 23:42:56 -0600

    learning array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+/*
+    Object Destructuring
+*/
+
 // const person = {
 //   name: "Brayan",
 //   age: 24,
@@ -18,14 +22,35 @@
 //   console.log(`It's ${temperature}°C in ${city}`);
 // }
 
-const book = {
-  title: "Ego  is the Enemy",
-  author: "Ryan Holiday",
-  publisher: {
-    name: "Penguin"
-  }
-};
+// const book = {
+//   title: "Ego  is the Enemy",
+//   author: "Ryan Holiday",
+//   publisher: {
+//     name: "Penguin"
+//   }
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher;
+// const { name: publisherName = "Self-Published" } = book.publisher;
 
-console.log(publisherName); // Penguin if exists, if not default = Self-Published
+// console.log(publisherName); // Penguin if exists, if not default = Self-Published
+
+/*
+    Array Destructuring
+*/
+
+const address = [
+  "1299 S Juniper Street",
+  "Philadelphia",
+  "Pennsylvania",
+  "19147"
+];
+
+const [, city, state = "New York"] = address;
+
+console.log(`You are in ${city} ${state}`);
+
+const item = ["Coffee (hot)", "$2.00", "$2.50", "$2.75"];
+
+const [coffee, , mediumCoffeePrice] = item;
+
+console.log(`A medium ${coffee} costs ${mediumCoffeePrice}`);
